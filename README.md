40bafb57952ea00289ecd4f7ccf4f094c2d88661
Unlawful Citizen <unlawfulcitizen@gmail.com>
2017-05-18 17:15:41 -0500

    MTTA Initial Load

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # MTTA
+
+=============
+
+![](logo.png)
+
+Chrome extension that replaces occurrences of Donald Trump with random insults.
+
+
+
+
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag MTTA.crx into the page that appears.
+
---
+++ Source/content_script.js
@@ -0,0 +1,47 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+	
+	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+	    || node.classList.indexOf('ace_editor') > -1) {
+		return;
+	}
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/\bdonald trump\b/g, "Agent Orange");
+	v = v.replace(/\bdonald Trump\b/g, "Hair Furher");
+	v = v.replace(/\bDonald trump\b/g, "Tangerine Cockholster");
+	v = v.replace(/\bDonald Trump\b/g, "Cheeto-In-Chief");
+	
+	textNode.nodeValue = v;
+}
+
+
